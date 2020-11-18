0a52702b6c98cf76f747e73480b3d92cddce3d6a
StevenVallarsa <orionsjv@gmail.com>
2020-11-18 17:10:55 -0500

    Finished simple version

--- app.js
+++ app.js
@@ -9,5 +9,31 @@ const quoteParts = {
         'Over many a quaint and curious volume of forgotten lore',
         'O lovely Pussy! O Pussy, my love, What a beautiful Pussy you are',
         'Old age should burn and rave at close of day',
-        '']
+        ''],
+    three: ['And that has made all the difference.',
+        'Daddy, daddy, you bastard, I’m through.',
+        'And drunk the milk of Paradise.',
+        'And my soul from out that shadow that lies floating on the floor shall be lifted—nevermore!',
+        'And—which is more—you’ll be a Man, my son!',
+        'Do not stand at my grave and cry, I am not there; I did not die.'
+        ]
+};
+
+/**
+ * function to pick random quote snippet
+ */
+const picker = index => {
+    const rnd = Math.floor(Math.random() * quoteParts[index].length);
+    console.log(rnd);
+    return quoteParts[index][rnd];
+};
+
+const parts = ['one', 'two', 'three'];
+let quote = '';
+
+for (let part of parts) {
+    quote += `${picker(part)},\n`;
 }
+
+quote = quote.substring(0, quote.length - 2);
+console.log(quote);
