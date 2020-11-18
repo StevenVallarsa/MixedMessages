3f881ab51ae2b0eceada715ab6b0df7d03ffda6b
StevenVallarsa <orionsjv@gmail.com>
2020-11-18 16:44:54 -0500

    First Commit

---
+++ READ_ME.md
@@ -0,0 +1,10 @@
+# Mixed Messages
+
+A random funny nonsensical quote generator project for the CodeAcademy Full Stack JavaScript Career Path program.
+
+### TOOLS
+
+1. Visual Studio Code
+2. Git
+3. Github
+4. Safari
---
+++ app.js
@@ -0,0 +1,13 @@
+const quoteParts = {
+    one: ['To be or not to be',
+        'It was the best of time, it was the worst of times',
+        'Once upon a midnight dreary, while I pondered weak and weary',
+        'Half a league, half a league, Half a league onward',
+        'How do I love thee? Let me count the ways',
+        'In Xanadu did Kubla Khan A stately pleasure-dome decree'],
+    two: ['All in the valley of Death',
+        'Over many a quaint and curious volume of forgotten lore',
+        'O lovely Pussy! O Pussy, my love, What a beautiful Pussy you are',
+        'Old age should burn and rave at close of day',
+        '']
+}
